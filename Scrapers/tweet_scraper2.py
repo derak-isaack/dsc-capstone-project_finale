5224306f6bd7fad1f5c231a4b845da0709f077e4
deerak-isaack <oderaisaack@gmail.com>
2024-02-06 01:19:02 +0300

    delete unnecessary cells

--- Scrapers/tweet_scraper2.py
+++ Scrapers/tweet_scraper2.py
@@ -13,8 +13,8 @@ def get_tweets(name, modes, no):
     return data 
 
 #Scrape EABL Tusker sentiments and store them in  a DataFrame.
-data = pd.DataFrame(get_tweets("eabl tusker until:2018-02-21 since:2006-08-01", "term", 75))
+data = pd.DataFrame(get_tweets("kenya cane until:2013-12-08 since:2006-10-02", "term", 50))
 
 data['date'] = pd.to_datetime(data['date'], format="%b %d, %Y · %I:%M %p %Z")
 
-data.to_csv("EABL_Tusker_sentiments.csv", index=False)
+data.to_csv("Kenya_Cane_sentiments.csv", index=False)

444b2c170cc760f921a429a89bd352750649ce0d
James Mwenda <mwendajames341@gmail.com>
2024-02-06 09:43:44 +0300

    updated

--- code/aquisition.py
+++ code/aquisition.py
@@ -122,3 +122,20 @@ data = pd.DataFrame(get_tweets("eabl tusker until:2018-02-21 since:2006-08-01", 
 data['date'] = pd.to_datetime(data['date'], format="%b %d, %Y · %I:%M %p %Z")
 
 data.to_csv("EABL_Tusker_sentiments.csv", index=False)
+
+
+def get_tweets(name, modes, no):
+    tweets = scraper.get_tweets(name, mode=modes, number=no)
+    final_tweets = []
+    for tweet in tweets['tweets']:
+        data = [tweet['text'],tweet['date'], tweet['stats']['likes'], tweet['stats']['comments']]
+        final_tweets.append(data)
+    data= pd.DataFrame(final_tweets, columns=['text','date','No_of_likes','No_of_tweets'])
+    return data 
+
+#Scrape EABL Tusker sentiments and store them in  a DataFrame.
+data = pd.DataFrame(get_tweets("tusker project fame until:2013-12-31 since:2006-11-10", "term", 80))
+
+data['date'] = pd.to_datetime(data['date'], format="%b %d, %Y · %I:%M %p %Z")
+
+data.to_csv("TuskerProjectFame_sentiments.csv", index=False)
